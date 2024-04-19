c3ad5a1d66e40b73dc151e2d02c17aceafda56b5
dolphin <1811909509@qq.com>
2024-04-19 20:56:29 +0800

    Update hardwareMap.py, add mouse control function

--- hardwareMap2Keyboard/hardwareMap.py
+++ hardwareMap2Keyboard/hardwareMap.py
@@ -9,17 +9,34 @@ def sendMsg(x):
     keyboard.send_data(lst[x])
     keyboard.release()
 
+def sendMouseMsg(dir, dis, x, y):
+    '''
+    Args:
+        dir: direction the mouse to move. Options: 0 -> left, 1 -> right.
+        dis: how many pixels the mouse will move.
+        (x, y): resolution of your screen.
+    '''
+    assert(dir==0 or dir==1)
+    lst = [-dis, dis]
+    mouse = ch9329Comm.mouse.DataComm(x,y)
+    mouse.send_data_relatively(lst[dir], 0) # not to change y value by default
+    # mouse.click()
 
 if __name__ == '__main__':
     serial.ser = serial.Serial('COM8', 115200)
     x = 0
     cnt = 0
+    resolution = {
+        "x": 1920,
+        "y": 1080
+    }
     while cnt<10:
         x += 1
         cnt += 1
-        x %= 3
+        x %= 2
         print(x)
         time.sleep(1)
-        sendMsg(x)
+        # sendMsg(x)
+        sendMouseMsg(x, 100, resolution["x"], resolution["y"])
 
     serial.ser.close()
